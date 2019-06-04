9b4c86eb4602ec578d07fcd6f26628ed495db108
Michail Bondarenko <mcmikius15@gmail.com>
2019-06-04 10:17:03 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Michail Bondarenko on 6/4/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
